861ff53534470eeb12f925a78e97ccdffdeeabb6
jrpacheco <pachecofaculdade@gmail.com>
2015-12-31 20:03:10 -0200

    Add AngularJS no projeto e criação de modulos e serviços

---
+++ Estudante/Estudante/Scripts/MyScripts/Service.js
@@ -0,0 +1,13 @@
+﻿
+app.service('EstudanteService', function ($http) {
+
+    // Cria um novo registro
+    this.post = function (Estudante) {
+        var request = $http({
+            method: 'POST',
+            url: 'api/EstudantesAPI',
+            data: Estudante
+        });
+        return request;
+    }
+});
