039606898f79c2366d2da482bfb54f3044cfe136
jrpacheco <pachecofaculdade@gmail.com>
2016-01-02 02:34:45 -0200

    Add database local

--- Estudante/Estudante/Scripts/MyScripts/Controller.js
+++ Estudante/Estudante/Scripts/MyScripts/Controller.js
@@ -4,7 +4,7 @@
 
     carregarRegistros();
 
-    function carregarRegistros() {        
+    function carregarRegistros() {
         var lista = EstudanteService.getEstudantes();
         lista.then(function (pl) {
             $scope.Estudantes = pl.data;
@@ -22,10 +22,64 @@
             $scope.Sobrenome = res.Sobrenome;
             $scope.Escola = res.Escola;
             $scope.DataNascimento = res.DataNascimento;
-            $scope.IsNewRecord = 0;
+            $scope.NovoRegistro = 0;
         },
-        function (errorPl) {
-            alert('Falha ao buscar registro', errorPl);
+        function (error) {
+            alert('Falha ao buscar registro', error);
         });
+    }
+
+    $scope.save = function () {
+        var Estudante = {
+            Id: $scope.Id,
+            Nome: $scope.Nome,
+            Sobrenome: $scope.Sobrenome,
+            Escola: $scope.Escola,
+            DataNascimento: $scope.DataNascimento
+        };
+
+        if ($scope.NovoRegistro === 1) {
+            var promisePost = EstudanteService.post(Estudante);
+            promisePost.then(function (pl) {
+                $scope.Id = pl.data.Id;
+                carregarRegistros();
+            }, function (erro) {
+                alert("Erro: " + erro.status + " " + erro.statusText);
+            });
+        } else {
+            var promisePut = EstudanteService.put($scope.Id, Estudante);
+            promisePut.then(function (pl) {
+                $scope.Message = "Registro atualizado com sucesso";
+                carregarRegistros();
+            }, function (erro) {
+                alert("Erro: " + erro.status + " " + erro.statusText);
+            });
+        }
+    };
+    
+    $scope.delete = function () {
+        var promiseDelete = EstudanteService.delete($scope.Id);
+        promiseDelete.then(function (pl) {
+            $scope.Message = "Deletado com sucesso";
+            $scope.Id = 0;
+            $scope.Nome = "";
+            $scope.Sobrenome = "";
+            $scope.Escola = "";
+            $scope.DataNascimento = "";
+
+            carregarRegistros();
+
+        }, function (erro) {
+            alert("Erro: " + erro.status + " " + erro.statusText);
+        });
+    }
+    
+    $scope.clear = function () {
+        $scope.NovoRegistro = 1;
+        $scope.Id = 0;
+        $scope.Nome = "";
+        $scope.Sobrenome = "";
+        $scope.Escola = "";
+        $scope.DataNascimento = "";        
     }
 });
